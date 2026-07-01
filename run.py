c58978df411ade20de9f8d4cfaf3bc7ec457dd20
ShoxaMajor <107797479+VanDeavTrd@users.noreply.github.com>
2026-07-01 16:46:51 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,25 @@
+import importlib.util
+import sys
+import os
+
+if __name__ == "__main__":
+    MODULE = "ShoxaGen"
+    
+    so_file = next((f for f in os.listdir('.') if f.startswith(MODULE) and (f.endswith('.so') or f.endswith('.pyd'))), None)
+    
+    if not so_file:
+        sys.exit("File kompilasi tidak ditemukan!")
+    
+    try:
+        spec = importlib.util.spec_from_file_location(MODULE, so_file)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[MODULE] = module
+        spec.loader.exec_module(module)
+        
+        if hasattr(module, 'make'):
+            module.make()
+        elif hasattr(module, 'main'):
+            module.main()
+            
+    except Exception as e:
+        sys.exit(f"Error: {e}")
